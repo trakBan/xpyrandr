79e52a12cc500b5a294dc7657673da4d921132ae
trak <trakxyz@tuta.io>
2022-09-03 17:32:24 +0200

    somewhat working version

--- parse.py
+++ parse.py
@@ -2,11 +2,13 @@ from globalVariables import *
 
 def parser(args: list) -> list:
 
+    # Prints help and then quits
     for x in args:
         if x in ("--help", "-h"):
             print(HELP)
             raise SystemExit()
 
+        # Prints the version and then quits
         if x in ("--version", "-v"):
             print("Version:", VERSION)
             raise SystemExit()
--- xpyrandr.py
+++ xpyrandr.py
@@ -2,6 +2,15 @@ from sys import argv
 from subprocess import PIPE, run
 from parse import parser
 
+# Wrapper for testing
+def wrapper(monitors: dict) -> dict:
+    for x in monitors:
+        print(f"{x} - {monitors[x]}")
+
+def finalXrandr(cmd: str) -> str:
+    print(cmd)
+    raise SystemExit()
+
 parser(argv[1:])
 
 raw_monitors: str = run(("xrandr --listmonitors").split(" "),
@@ -25,8 +34,8 @@ for y, x in enumerate(raw_xrandr.splitlines()):
             splitted_line = (raw_xrandr.splitlines()[y + 1].strip().split())
 
             # Adds highest resolution
-            monitors[z] = [splitted_line[0]]
-
+            monitors[z].append(splitted_line[0])
+ 
             # Adds highest refresh rate
             for w in splitted_line[1:]:
                 w = w.strip().translate({ord(letter): None for letter in "-+*"})
@@ -35,4 +44,24 @@ for y, x in enumerate(raw_xrandr.splitlines()):
 
             monitors[z].append(max(splitted_line_hz))
 
-print(monitors)
+for y, x in enumerate(monitors):
+    if y == 0:
+        monitors[x] = (f"xrandr --output {x} --primary --size {monitors[x][0]} --rate {monitors[x][1]}")
+    else:
+        monitors[x] = (f"--output {x} --size {monitors[x][0]} --rate {monitors[x][1]}")
+
+if len(monitors) == 1:
+    finalXrandr(monitors[list(monitors)[0]])
+
+if len(monitors) > 2:
+    print("Currently this programm only supports up to 2 monitors!")
+    raise SystemExit()
+
+side: str = input("(left) or (right) based on the primary monitor: ").lower()
+monitors[list(monitors)[1]] += f" --{side}-of {list(monitors)[0]}"
+
+cmd: str = ""
+for x in monitors:
+    cmd += monitors[x] + " "
+
+finalXrandr(cmd)
